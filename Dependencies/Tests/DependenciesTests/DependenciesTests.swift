bba88ddb75ce22cd5038b564d3fa0b6cf79cbaed
Tsui <starkrimson@icloud.com>
2022-06-30 17:59:55 +0800

    第三方框架

---
+++ Dependencies/Tests/DependenciesTests/DependenciesTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Dependencies
+
+final class DependenciesTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Dependencies().text, "Hello, World!")
+    }
+}
