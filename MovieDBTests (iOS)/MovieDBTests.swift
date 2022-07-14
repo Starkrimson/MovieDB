04c5039834e1080f924ba9db31f742fef40d9fcb
Tsui <starkrimson@icloud.com>
2022-07-14 17:39:47 +0800

    测试获取详情

--- MovieDBTests (iOS)/MovieDBTests.swift
+++ MovieDBTests (iOS)/MovieDBTests.swift
@@ -19,21 +19,60 @@ final class MovieDBTests: XCTestCase {
         )
         
         store.environment.dbClient.popular = {
-            Effect(value:  $0 == .movie ? mockMovies : mockTVShows)
+            Effect(value:  $0 == .movie ? mockMediaMovies : mockMediaTVShows)
         }
         
         // 获取热门电影
         store.send(.fetchPopular(.movie))
         // 成功接收到热门电影
-        store.receive(.fetchPopularDone(kind: .movie, result: .success(mockMovies))) {
-            $0.popularMovies = .init(uniqueElements: mockMovies)
+        store.receive(.fetchPopularDone(kind: .movie, result: .success(mockMediaMovies))) {
+            $0.popularMovies = .init(uniqueElements: mockMediaMovies)
         }
         
         // 获取热门电视节目
         store.send(.fetchPopular(.tv))
         // 成功接收热门电视节目
-        store.receive(.fetchPopularDone(kind: .tv, result: .success(mockTVShows))) {
-            $0.popularTVShows = .init(uniqueElements: mockTVShows)
+        store.receive(.fetchPopularDone(kind: .tv, result: .success(mockMediaTVShows))) {
+            $0.popularTVShows = .init(uniqueElements: mockMediaTVShows)
         }
+    }
+    
+    func testFetchDetails() {
+        let store = TestStore(
+            initialState: .init(media: mockMedias[0]),
+            reducer: detailReducer,
+            environment: .init(mainQueue: .immediate, dbClient: .failing)
+        )
+        
+        store.environment.dbClient.details = { mediaType, _ in
+            switch mediaType {
+            case .all:
+                return Effect(error: .sample("Something Went Wrong"))
+            case .movie:
+                return Effect(value: .movie(mockMovies[0]))
+            case .tv:
+                return Effect(value: .tv(mockTVShows[0]))
+            case .person:
+                return Effect(value: .person(mockPeople[0]))
+            }
+        }
+        
+        store.send(.fetchDetails(mediaType: .movie))
+        store.receive(.fetchDetailsDone(.success(.movie(mockMovies[0])))) {
+            $0.movie = mockMovies[0]
+        }
+        
+        store.send(.fetchDetails(mediaType: .tv))
+        store.receive(.fetchDetailsDone(.success(.tv(mockTVShows[0])))) {
+            $0.tv = mockTVShows[0]
+        }
+        
+        store.send(.fetchDetails(mediaType: .person))
+        store.receive(.fetchDetailsDone(.success(.person(mockPeople[0])))) {
+            $0.person = mockPeople[0]
+        }
+        
+        store.send(.fetchDetails(mediaType: .all))
+        store.receive(.fetchDetailsDone(.failure(.sample("Something Went Wrong"))))
     }
 }
--- Shared/Model/Mock.swift
+++ Shared/Model/Mock.swift
@@ -9,7 +9,7 @@ import Foundation
 
 #if DEBUG
 
-let mockMovies: [Media] = [
+let mockMediaMovies: [Media] = [
     .init(
         title: "小黄人大眼萌2：格鲁的崛起",
         originalTitle: "Minions: The Rise of Gru",
@@ -27,7 +27,7 @@ let mockMovies: [Media] = [
     ),
 ]
 
-let mockTVShows: [Media] = [
+let mockMediaTVShows: [Media] = [
     .init(
         name: "Stranger Things",
         originalName: "Stranger Things",
@@ -45,7 +45,7 @@ let mockTVShows: [Media] = [
     ),
 ]
 
-let mockMedias : [Media] = [
+let mockMedias: [Media] = [
     .init(
         title: "雷神4：爱与雷霆",
         originalTitle: "Thor: Love and Thunder",
@@ -65,6 +65,48 @@ let mockMedias : [Media] = [
         profilePath: "/edPU5HxncLWa1YkgRPNkSd68ONG.jpg",
         mediaType: .person,
         id: 524
+    ),
+]
+
+let mockMovies: [Movie] = [
+    .init(
+        id: 616037,
+        originalTitle: "Thor: Love and Thunder",
+        posterPath: "/i1NAuDW8oOlV5dBbXPPTuPlt8sl.jpg",
+        title: "雷神4：爱与雷霆",
+        voteAverage: 7
+    ),
+    .init(
+        id: 453395,
+        originalTitle: "Doctor Strange in the Multiverse of Madness",
+        posterPath: "/fCNNXrGvGi4EDlzZgPOGVdkgtuc.jpg",
+        title: "奇异博士2：疯狂多元宇宙",
+        voteAverage: 7
+    ),
+    .init(
+        id: 438148,
+        originalTitle: "Minions: The Rise of Gru",
+        posterPath: "/hCie8qIrmMsEvPxDdvqJczj82n9.jpg",
+        title: "小黄人大眼萌2：格鲁的崛起",
+        voteAverage: 7
+    ),
+]
+
+let mockTVShows: [TVShow] = [
+    .init(
+        id: 66732,
+        name: "Stranger Things",
+        originalName: "Stranger Things",
+        posterPath: "/2mYLTEQd1oHuXA6NICPIKX7OOvo.jpg",
+        voteAverage: 8
+    ),
+]
+
+let mockPeople: [Person] = [
+    .init(
+        id: 524,
+        name: "Natalie Portman",
+        profilePath: "/edPU5HxncLWa1YkgRPNkSd68ONG.jpg"
     ),
 ]
 
