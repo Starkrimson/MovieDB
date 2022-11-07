2fa3f9c16ba03407bf3b29aae27025d2b6b1b39d
Tsui <starkrimson@icloud.com>
2022-11-07 13:55:14 +0800

    更新第三方框架

--- MovieDependencies/Package.swift
+++ MovieDependencies/Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Dependencies",
+    name: "MovieDependencies",
     platforms: [
         .iOS(.v15),
         .macOS(.v12)
@@ -12,26 +12,26 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Dependencies",
-            targets: ["Dependencies"]),
+            name: "MovieDependencies",
+            targets: ["MovieDependencies"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.39.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.45.0"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Dependencies",
+            name: "MovieDependencies",
             dependencies: [
                 .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
                 .product(name: "Kingfisher", package: "Kingfisher")
             ]),
         .testTarget(
-            name: "DependenciesTests",
-            dependencies: ["Dependencies"]),
+            name: "MovieDependenciesTests",
+            dependencies: ["MovieDependencies"]),
     ]
 )
