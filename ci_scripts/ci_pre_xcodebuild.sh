d634331202070d2cfdd12d3a8de04b2e2dc55348
Tsui <starkrimson@icloud.com>
2023-11-30 12:17:07 +0800

    ComposableArchitectureMacros must be enabled before it can be used.

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -5,3 +5,6 @@ brew install swiftlint
 
 # 设置 api key
 /usr/libexec/PlistBuddy -c "Set :api_key ${API_KEY}" "$CI_WORKSPACE/Shared/Info.plist"
+
+# 'ComposableArchitectureMacros' must be enabled before it can be used.
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
